3acf4bd143341ade139337d76f29b4cfbe1d5cbe
WadOmar <47010697+WadOmar@users.noreply.github.com>
2019-10-24 09:03:05 +0000

    Add files via upload

---
+++ code.c
@@ -0,0 +1,16 @@
+#include "../lib/libgraphique.h"
+#include<stdio.h>
+
+int main(void)
+    {
+    ouvrir_fenetre(1400,900);
+
+    
+    //fin du programme
+    attendre_clic() ;
+    fermer_fenetre() ;
+    return 0 ;
+    }
+
+
+
