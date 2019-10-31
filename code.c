087d2dd19528e39ee6042ed577084844274c6442
den1337lol <56869643+den1337lol@users.noreply.github.com>
2019-10-31 17:36:37 +0100

    Add files via upload

--- code.c
+++ code.c
@@ -2,23 +2,25 @@
 #include<stdio.h>
 #include<math.h>
 
-void dessiner_segment (Point p1, Point p2);
+/*void dessiner_segment (Point p1, Point p2);
 void dessiner_triangle (Point p1, Point p2, Point p3);
 void dessiner_rect (Point p1, Point p2, Point p3, Point p4);
-/*void dessiner_rectp(Point p1, int largeur, int hauteur, Couleur );
-void dessiner_disque(Point p1, int rayon, Couleur );*/
+void dessiner_disquer(Point p1, int rayon, Couleur couleur);*/
+void dessiner_rectangler(Point p1, int largeur, int hauteur, Couleur couleur) ;
 
 int main(void)
     {
     //indispensable pour commencer
     ouvrir_fenetre(1400,900);
 
-	int largeur, hauteur;	
+	int largeur, hauteur, rayon;	
 	Point p1, p2, p3, p4;
-	dessiner_segment(p1,p2);
+	/*dessiner_segment(p1,p2);*/
 	/*dessiner_triangle(p1,p2,p3);*/
 	/*dessiner_rect(p1,p2,p3,p4);*/
-    
+	/*dessiner_disquer(p1,rayon,rouge);*/
+	dessiner_rectangler(p1, largeur, hauteur, vert);    
+
     //fin du programme
     attendre_clic() ;
     fermer_fenetre() ;
@@ -26,13 +28,13 @@ int main(void)
     }
 
 
-void dessiner_segment(Point p1, Point p2)
+/*void dessiner_segment(Point p1, Point p2)
 	{
 		p1 = attendre_clic();
 		p2 = attendre_clic();			
 		dessiner_ligne(p1,p2, vert);
 		actualiser();	
-	}
+	}*/
 
 /*void dessiner_triangle(Point p1, Point p2, Point p3)
 	{
@@ -61,4 +63,47 @@ void dessiner_segment(Point p1, Point p2)
 		
 	}*/
 
+/*void dessiner_disquer (Point p1, int rayon, Couleur couleur)
+	{
+	p1 = attendre_clic();
+	Point p2 = attendre_clic();
+	rayon = sqrt(pow((p2.x - p1.x),2) + pow((p2.y - p1.y),2));
+	dessiner_disque (p1, rayon, couleur);
+	actualiser();
+	}*/	
 
+void dessiner_rectangler (Point p1, int largeur, int hauteur, Couleur couleur)
+	{
+	p1 = attendre_clic();
+	Point p2 = attendre_clic();
+	Point coin;
+	if (p1.x < p2.x && p1.y < p2.y)	
+		{		
+		largeur = p2.x - p1.x;
+		hauteur = p2.y - p1.y;
+		dessiner_rectangle (p1, largeur, hauteur, vert);
+		}
+	else if (p1.x < p2.x && p1.y > p2.y)
+		{
+		largeur = p2.x - p1.x;
+		hauteur = p1.y - p2.y;
+		coin.x = p1.x; 
+		coin.y = p2.y;
+		dessiner_rectangle (coin, largeur, hauteur, vert);
+		}
+	else if (p1.x > p2.x && p1.y < p2.y)
+		{
+		largeur = p1.x - p2.x;
+		hauteur = p2.y - p1.y;
+		coin.x = p2.x; 
+		coin.y = p1.y;
+		dessiner_rectangle (coin, largeur, hauteur, vert);
+		}
+	else 
+		{
+		largeur = p1.x - p2.x;
+		hauteur = p1.y - p2.y;
+		dessiner_rectangle (p2, largeur, hauteur, vert);
+		}
+	actualiser();
+	}
