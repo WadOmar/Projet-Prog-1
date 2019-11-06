6323f51676c880e6079ce7918acaa29328dd905f
WadOmar <47010697+WadOmar@users.noreply.github.com>
2019-11-06 09:07:16 +0000

    Add files via upload

--- code.c
+++ code.c
@@ -15,6 +15,7 @@
 #define NBOUTILS 1
 
 // Prototypes des fonctions
+void initialisation(void) ;
 int dansDessin(Point posSouris) ;
 void bouton(Point posSouris, int x, int y, int l, int h, Couleur cb, Couleur cs, int outilSelect) ;
 void fill (Couleur couleur) ;
@@ -25,10 +26,13 @@ void gestionOutils(void) ;
 //void texte()
 void dessinBoutons(void) ;
 
-void cercleVide() ;
+void segment(void) ;
+void rectangleVide(void) ;
+void cercleVide(void) ;
+void polygoneVide(void) ;
 // Déclaration des variables globales
 
-int outil = -1;
+static int outil = 2;
 
 typedef enum
 	{
@@ -48,14 +52,13 @@ int main(int argc, char *argv[])
 	
 	chargement() ;
 	//menu() ;
-	
+	initialisation();
 	int dansPaint = 1 ;
 	
 	while (dansPaint == 1)
 		{
-		affichage() ;
-		dessinBoutons() ;
-		gestionOutils() ;
+			affichage();
+			gestionOutils();
 		}
 	
 	//fin du programme
@@ -75,19 +78,23 @@ void fill(Couleur couleur)
 
 void bouton(Point posSouris, int x, int y, int l, int h, Couleur cb, Couleur cs, int outilSelect)
 	{
-	extern int outil ;
 	Point coin = {x, y}, clic ;
 
 	clic = clic_a_eu_lieu() ;
-	
+
+
 	if (posSouris.x > coin.x && posSouris.x < coin.x + l && posSouris.y > coin.y && posSouris.y < coin.y + h)
 		{
-		dessiner_rectangle(coin, l, h, cs) ;
+		printf("(%d, %d) (%d, %d)\n", x, y, clic.x, clic.y);
+		dessiner_rectangle(coin, l, h, cs);
 		
 		if (clic.x != -1 && clic.y != -1)
 			{
 			outil = outilSelect ;
 			printf("clic !\n") ;
+			
+			if (outilSelect == 666)
+				initialisation () ;
 			}
 		}
 	else 
@@ -127,53 +134,51 @@ void menu(void)
 		}
 	}
 */
-
-void affichage(void)
+void initialisation(void)
 	{
 	Point coin0 = {0,0}, coin1 = {0,150}, coin2 = {ZONE_DESSIN_LONGUEUR, 0} ;
 	
 	fill (navajowhite) ;
 	dessiner_rectangle(coin0, ZONE_DESSIN_LONGUEUR, TAB_HAUT, gris) ;
 	dessiner_rectangle(coin1, ZONE_DESSIN_LONGUEUR, ZONE_DESSIN_LARGEUR, blanc) ;
-	dessiner_rectangle(coin2, TAB_DROITE, TAB_HAUT, rouge) ;
+	dessiner_rectangle(coin2, TAB_DROITE, TAB_HAUT, rouge) ;	
+	}
 
+void affichage(void)
+	{
+	dessinBoutons();
 	actualiser() ;
 	}
 
 void dessinBoutons(void)
 	{
-	extern int outil ;
-
 	traiter_evenements() ;
 	Point posSouris = deplacement_souris_a_eu_lieu() ;
+	
+	bouton(posSouris, ZONE_DESSIN_LONGUEUR, 0, TAB_DROITE, TAB_HAUT, darkred, red, 666) ;	
+	
 	bouton(posSouris, ZONE_DESSIN_LONGUEUR, TAB_HAUT, 50, 50, violet, violetlight, (int)SEGMENT) ;
 	bouton(posSouris, ZONE_DESSIN_LONGUEUR, TAB_HAUT + 100, 50, 50, violet, violetlight, (int)RECTANGLE_VIDE) ;
 	bouton(posSouris, ZONE_DESSIN_LONGUEUR, TAB_HAUT + 200, 50, 50, violet, violetlight, (int)CERCLE_VIDE) ;
 	bouton(posSouris, ZONE_DESSIN_LONGUEUR, TAB_HAUT + 300, 50, 50, violet,	violetlight, (int)POLYGONE_VIDE) ;
-	
 	reinitialiser_evenements() ;
-
-	actualiser() ;
 	}
 
 void gestionOutils(void)
 	{
-	extern int outil ;
-	
 	switch (outil)
 		{
 		case 0 :
-			printf("segment\n") ;	
-			//dessinerSegment();
+			segment() ;
 			break ;		
 		case 1 :
-			printf("rectangle Vide\n") ;
+			rectangleVide() ;
 			break ;
 		case 2 :
-			printf("cercle vide\n") ;
+			cercleVide() ;
 			break ;
 		case 3 :
-			printf("polygone\n") ;
+			polygoneVide() ;
 			break ;
 		}
 	}
@@ -184,4 +189,37 @@ int dansDessin(Point posSouris)
 		return 1 ;
 	else
 		return 0 ;
+	}
+
+void segment (void)
+	{
+	Point p1, p2 ;	
+	p1 = attendre_clic();
+	p2 = attendre_clic();			
+	dessiner_ligne(p1,p2, vert);
+	}
+
+void rectangleVide (void)
+	{
+	Point p1, p2, p3, p4;
+	p1 = attendre_clic();
+	p2 = attendre_clic();
+	p3.x = p2.x;
+	p3.y = p1.y;
+	p4.x = p1.x;
+	p4.y = p2.y;
+	dessiner_ligne(p1, p3, bleu);
+	dessiner_ligne(p1, p4, bleu);
+	dessiner_ligne(p2, p4, bleu);
+	dessiner_ligne(p3, p2, bleu);
+	}
+
+void cercleVide(void)
+	{
+
+	}
+
+void polygoneVide(void)
+	{
+
 	}
